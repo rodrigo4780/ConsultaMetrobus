a2d0b1e1cfc44ab40e667b7e9a4a1b210e80973d
Rodrigo Martinez Cruz <rodrigo4780@hotmail.com>
2020-09-27 14:22:20 -0500

    Se agregan comentarios

--- ServicioMetrobus/Controllers/AlcaldiasController.cs
+++ ServicioMetrobus/Controllers/AlcaldiasController.cs
@@ -14,31 +14,42 @@ namespace ServicioMetrobus.Controllers
     [ApiController]
     public class AlcaldiasController : ControllerBase
     {
+        //Obtener una lista de las Alcaldias Disponibles
         // GET: api/Alcaldias/get
         [HttpGet]
         public string Get()
         {
+            //Se instancia un DataTable donde guardaremos el resultado de la consulta
             DataTable dt = new DataTable();
 
+            //Mandamos a llamar el metodo que consultara la base de datos.
             dt = PostgresUtility.ConsultaAlcaldias();
 
+            //Se crea string para asignar el Json resultante
             string JSONString = string.Empty;
+
+            //Serializamos el contenido del DataTable a Json
             JSONString = JsonConvert.SerializeObject(dt);
 
             return JSONString;
 
         }
 
-
+        //Obtener una lista de unidades que hayan estado dentro de una alcaldia, como parametro mandamos la clave de alcaldia a consultar.
         // GET: api/Alcaldias/get/17
         [HttpGet("{id}")]
         public string Get(string id)
         {
+            //Se instancia un DataTable donde guardaremos el resultado de la consulta
             DataTable dt = new DataTable();
 
+            //Mandamos a llamar el metodo que consultara la base de datos.
             dt = PostgresUtility.ConsultaAlcaldias(id);
 
+            //Se crea string para asignar el Json resultante
             string JSONString = string.Empty;
+
+            //Serializamos el contenido del DataTable a Json
             JSONString = JsonConvert.SerializeObject(dt);
 
             return JSONString;
@@ -50,29 +61,41 @@ namespace ServicioMetrobus.Controllers
     [ApiController]
     public class UnidadesController : ControllerBase
     {
+        //Obtener una lista de las unidades disponibles
         // GET: api/Unidades/get
         [HttpGet]
         public string Get()
         {
+            //Se instancia un DataTable donde guardaremos el resultado de la consulta
             DataTable dt = new DataTable();
 
+            //Mandamos a llamar el metodo que consultara la base de datos.
             dt = PostgresUtility.ConsultaUnidades();
 
+            //Se crea string para asignar el Json resultante
             string JSONString = string.Empty;
+
+            //Serializamos el contenido del DataTable a Json
             JSONString = JsonConvert.SerializeObject(dt);
 
             return JSONString;
         }
 
+        //Obtener el historial de ubicaciones/fechas de una unidad dado su Id
         // GET: api/Unidades/get/497
         [HttpGet("{id}")]
         public string Get(int id)
         {
+            //Se instancia un DataTable donde guardaremos el resultado de la consulta
             DataTable dt = new DataTable();
 
+            //Mandamos a llamar el metodo que consultara la base de datos.
             dt = PostgresUtility.ConsultaUnidades(id);
 
+            //Se crea string para asignar el Json resultante
             string JSONString = string.Empty;
+
+            //Serializamos el contenido del DataTable a Json
             JSONString = JsonConvert.SerializeObject(dt);
 
             return JSONString;
